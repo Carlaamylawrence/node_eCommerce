910f6d05b2a4b9e6644ca9c0474057eff0c8df87
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-07-27 15:40:14 +0200

    edit users

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const con = require("../lib/db_connection");
 
+// GET ALL USERS
 router.get("/", (req, res) => {
   try {
     con.query("SELECT * FROM users", (err, result) => {
@@ -13,6 +14,7 @@ router.get("/", (req, res) => {
   }
 });
 
+//ADD A USER
 router.post("/", (req, res) => {
   const {
     email,
@@ -37,13 +39,44 @@ router.post("/", (req, res) => {
   }
 });
 
+// GET SINGLE USER
 router.get("/:id", (req, res) => {
   try {
-    res.send({ id: req.params.id });
+    con.query(
+      `SELECT * FROM users where user_id= ${req.params.id} `,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
   }
 });
 
+//EDIT A USER
+router.put("/:id", (req, res) => {
+  const {
+    email,
+    password,
+    full_name,
+    billing_address,
+    default_shipping_address,
+    country,
+    phone,
+    user_type,
+  } = req.body;
+  try {
+    con.query(
+      `UPDATE users SET email="${email}", password="${password}", full_name="${full_name}", billing_address="${billing_address}", default_shipping_address="${default_shipping_address}", country="${country}", phone="${phone}", user_type="${user_type}" WHERE user_id= ${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = router;
