2fc2e754302fea2461b92ef0490c13ace0151440
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-08-04 10:11:19 +0200

    show data

--- controller/display/index.js
+++ controller/display/index.js
@@ -15,32 +15,6 @@ async function getUsers(req, res) {
   }
 }
 
-function show(data) {
-  let table = `<tr>
-                <th>full_name</th>
-                <th>password</th>
-                <th>email</th>
-                <th>billing_address</th>
-                <th>default_shipping_address</th>
-                <th>country</th>
-                <th>phone</th>
-                <th>user_type</th>
-                </tr>`;
-  for (let r of data) {
-    table += ` <tr>
-                    <td> ${r.full_name}</td>
-                    <td> ${r.password}</td>
-                    <td> ${r.email}</td>
-                    <td> ${r.billing_address}</td>
-                    <td> ${r.default_shipping_address}</td>
-                    <td> ${r.country}</td>
-                    <td> ${r.phone}</td>
-                    <td> ${r.user_type}</td>
-                    </tr>`;
-  }
-  document.getElementById("users").innerHTML = table;
-}
-
 // GET A SINGLE USER
 async function getSingleUser(req, res) {
   try {
